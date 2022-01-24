29d65493fa7a9ba29f869346cc8d1dfcb694ee12
KimrAndrew <andrewrk1997@gmail.com>
2022-01-23 20:17:32 -0800

    thead styled

--- components/ReportTable.js
+++ components/ReportTable.js
@@ -3,22 +3,25 @@ import ReportTableRow from './ReportTableRow';
 export default function ReportTable(props) {
     if (props.cookieStands.length > 0) {
         return (
-        <div className="text-center">
-            <table>
+            <table className='m-auto w-8/12'>
                 <thead>
-                    <tr>
-                        <th>Location</th>
-                        {props.hoursOfOperation.map(hour => <td key={hour}>{hour}</td>)}
+                    <tr className='bg-emerald-500 font-bold'>
+                        <th className='px-12'>Location</th>
+                        {props.hoursOfOperation.map(hour => <td className = 'px-2'key={hour}>{hour}</td>)}
                         <th>Totals</th>
                     </tr>
                 </thead>
                 <tbody>
-                        {props.cookieStands.map(location => {
-                            return <ReportTableRow key={location['location']} location={location}/>
+                        {props.cookieStands.map((location,i) => {
+                            if(i % 2 === 0) {
+                                let color = 'bg-emerald-200'
+                            } else {
+                                let color = 'bg-emerald-300'
+                            }
+                            return <ReportTableRow key={i} location={location}/>
                         })}
                 </tbody>
             </table>
-        </div>
         );
     }
     return (
